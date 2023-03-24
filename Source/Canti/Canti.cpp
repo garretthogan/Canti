133e823f7b72590a91b937ae19770ddee663a0be
garretthogan <garrett.hogan.dev@gmail.com>
2023-03-24 08:40:50 -0700

    init

---
+++ Source/Canti/Canti.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Canti.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Canti, "Canti" );
